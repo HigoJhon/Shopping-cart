539a65783b86d9a83c5fd08b8bf633d50c57db12
HigoJhon <higojhon98@gmail.com>
2022-10-12 12:45:54 -0300

    Criando test para a function fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,14 @@
-const fetchProducts = () => {
+const fetchProducts = async (pesquisa) => {
   // seu código aqui
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${pesquisa}`;
+
+  try {
+    const resul = await fetch(url)
+    const data = await resul.json();
+    return data;
+  } catch (err) {
+    return 'You must provide an url';
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,31 @@
 require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
+const item = require('../mocks/item');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it("testando se é uma função", () => {
+    expect(typeof fetchProducts).toBe('function');
+  })
+  it("testando caso passado computador como argumento da funcão, fetch é chamada", async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  })
+  it("testando caso passado computador como argumento da função, fetch utilaza o endpoint", async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith("https://api.mercadolibre.com/sites/MLB/search?q=computador")
+  })
+  it("testando caso passado computador como argumento da função, é uma estrutura de dados igual ao objeto", async () => {
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  })
+  it("Caso chame a função sem argumente retonar error", async () => {
+    await fetchProducts();
+    try {
+      expect(fetchProducts).toBe('You must provide an url');
+    } catch (error) {
+      error
+    }
+  })
+  // fail('Teste vazio');
 });
